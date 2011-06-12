acc42f78e7eb3060afe66d6c7df75571f312b9c3
ssriram <naughtysriram@gmail.com>
2011-06-13 01:53:19 +0530

    updated module functions

--- xyz.c
+++ xyz.c
@@ -27,7 +27,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 #define XYZCVERSION 1.0
-#define XYZCUPDATED june-06-2011
+#define XYZCUPDATED june-13-2011
 
 /*
 */
@@ -121,6 +121,7 @@ z *cons(z *pcar, z *pcdr) //cons a.k.a make_pair
     return p;
 }*/
 
+
 void splitmodobj(char *name, char schar, char *modname, char *objname)
 {
     int i = 0;
@@ -144,6 +145,7 @@ void splitmodobj(char *name, char schar, char *modname, char *objname)
         else
         {
             objname[j++] = name[i];
+            objname[j]='\0';
         }
     i++;
     }
@@ -159,57 +161,120 @@ z *findobj(z *objs, char *name)
     {
         if(strcmp(car(sym)->val.s,name)==0)
         {
+            //obj found
 			printf("%u %s at %d\n",(unsigned)car(sym),car(sym)->val.s,idx);
             return car(sym);
         }
         sym=cdr(sym);
 		idx++;
     }
-    p=make_obj();
-    TYPESET(p,tsymbol);
-    p->val.s=malloc(strlen(name)+1);
-    if(!p->val.s){fprintf(stderr,"problem: insufficient memory\n");exit(1);}
-    strcpy(p->val.s,name);
-    objs=cons(objs,p);
-    return p;
+    
+    //obj not found
+    return obj_notok;
+}
+
+z *createobj(z *objs, char *name)
+{
+    z *obj;
+    if(objs && name)
+    {
+        obj=make_obj();
+        TYPESET(obj,tsymbol);
+        obj->val.s=malloc(strlen(name)+1);
+        if(!obj->val.s){fprintf(stderr,"problem: insufficient memory\n");exit(1);}
+        strcpy(obj->val.s,name);
+        objs=cons(objs,obj);
+        return obj;
+    }
+    print("error: invalid objname / memory access\n");
+    return obj_notok;
 }
 
 z *make_symbol(z *scope, char *name)
 {
+
+/*
+if modname present in name
+    if module present
+        if obj present return obj
+        else throw error
+    else throw error
+else
+    if main module present
+        if obj present return obj
+        else create obj
+    else throw error    
+*/
+
     char modname[30],objname[30];
     z *obj, *tray, *currmod, *currobjs;
-    splitmodobj(name,':',modname,objname);
-    if(modname)
+
+    if(strchr(name,':')!=NULL)
     {
-        tray=gtray;
-        while(!is_null(tray))
+        splitmodobj(name,':',modname,objname);
+        
+        if(modname)
         {
-            currmod=car(tray);
-            if(strcmp(car(currmod).val->s,modname)==0)
+            //modname present
+            tray=gtray;
+            while(!is_null(tray))
             {
-                currobjs=cdr(currmod);
-                return findobj(currobjs,objname);
+                currmod=car(tray);
+                if(strcmp(car(currmod).val->s,modname)==0)
+                {
+                    //module present
+                    currobjs=cdr(currmod);
+                    obj= findobj(currobjs,objname);
+                    if(obj!=obj_notok)
+                    {
+                        //obj present
+                        return obj;
+                    }
+                    else
+                    {
+                        fprintf(stderr,"error: undefined object\n");                        
+                        return obj_notok;
+                    }
+                }
+                tray=cdr(tray);
             }
-            tray=cdr(tray);
+            fprintf(stderr,"error: undefined module\n");
+            return obj_notok;
         }
-        fprintf(stderr,"error: undefined module\n");
-        return obj_notok;
+        else
+        {
+            fprintf(stderr,"error: undefined module or object\n");
+            obj= obj_notok;
+        }        
     }
     else
     {
+        //no module name present
         tray=gtray;
         while(!is_null(tray))
         {
             currmod=car(tray);
             if(strcmp(car(currmod).val->s,"main")==0))
             {
+                //main module present
                 currobjs=cdr(currmod);
-                return findobj(currobjs,objname);
+                obj= findobj(currobjs,name);
+                if(obj!=obj_notok)
+                {
+                    //obj present
+                    return obj;
+                }
+                else
+                {
+                    //obj not present so create it.
+                    obj= createobj(currobjs,name);
+                    return obj;
+                }
             }
             tray=cdr(tray);
-        }
+        }        
         fprintf(stderr,"error: main module not found\n");
-        return obj_notok
+        return obj_notok;
     }
 }
 
